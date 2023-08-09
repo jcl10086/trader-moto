fc2a4efaeaea1f1162cf20f3ae93a444896a8633
jcl <1427682825@qq.com>
2023-08-09 16:12:51 +0800

    优化代码  已当日最新成交作为数据

--- money/core.py
+++ money/core.py
@@ -5,13 +5,15 @@ from pytdx.hq import TdxHq_API
 
 import easytrader
 
-user = None
+# 初始化账号
+user = easytrader.use('eastmoney')
+user.prepare('account.json')
 
 
-def get_user():
-    global user
-    user = easytrader.use('eastmoney')
-    user.prepare('account.json')
+# def get_user():
+#     global user
+#     user = easytrader.use('eastmoney')
+#     user.prepare('account.json')
 
 
 # 获取可以资金
@@ -32,35 +34,47 @@ def buy_info():
     print()
 
 
+# 获取当日成交
+def current_deal_info():
+    # 获取最新买入 stock_code 代码  cost_price 成本价  enable_amount 可用数量
+    current_deal = user.current_deal[0]
+    if current_deal.bs_type == 'B':
+        stock_code = current_deal.stock_code
+        cost_price = current_deal.deal_price
+        enable_amount = current_deal.deal_amount
+        my_position = {'stock_code': stock_code, 'cost_price': cost_price, 'enable_amount': enable_amount}
+        return my_position
+
+
 # 获取持仓
 # stock_code 代码  cost_price 成本价  enable_amount 可用数量
-def position_info():
-    positions = user.position
-    enable_amount = 0
-    for position in positions:
-        # 使用正则表达式解析字符串
-        pattern = r"(\w+)\s*=\s*([\w.'\d]+)"
-        matches = re.findall(pattern, str(position))
-        for match in matches:
-            if match[0] == 'enable_amount':
-                enable_amount = int(match[1])
-            if match[0] == 'stock_code':
-                stock_code = eval(match[1])
-            if match[0] == 'cost_price':
-                cost_price = eval(match[1])
-        if enable_amount != 0:
-            break
-    if enable_amount != 0:
-        my_position = {'stock_code': stock_code, 'cost_price': cost_price, 'enable_amount': enable_amount}
-        # print(my_position)
-        return my_position
-    else:
-        return None
+# def position_info():
+#     positions = user.position
+#     enable_amount = 0
+#     for position in positions:
+#         # 使用正则表达式解析字符串
+#         pattern = r"(\w+)\s*=\s*([\w.'\d]+)"
+#         matches = re.findall(pattern, str(position))
+#         for match in matches:
+#             if match[0] == 'enable_amount':
+#                 enable_amount = int(match[1])
+#             if match[0] == 'stock_code':
+#                 stock_code = eval(match[1])
+#             if match[0] == 'cost_price':
+#                 cost_price = eval(match[1])
+#         if enable_amount != 0:
+#             break
+#     if enable_amount != 0:
+#         my_position = {'stock_code': stock_code, 'cost_price': cost_price, 'enable_amount': enable_amount}
+#         # print(my_position)
+#         return my_position
+#     else:
+#         return None
 
 
 # 挂单卖出
 def sell_info():
-    my_position = position_info()
+    my_position = current_deal_info()
     if my_position is not None:
         online_one_price(my_position)
 
@@ -79,6 +93,8 @@ def online_one_price(my_position):
         if my_dicts is None:
             market_code = 1
 
+        # 保本价
+        bb_price = 0.0
         # 当前最大价
         max_price = 0.0
         while True:
@@ -91,6 +107,10 @@ def online_one_price(my_position):
                         price = round(float(value) / 100, 3)
                         diff_yk = round((price - cost_price) / cost_price * 100, 2)
                         print('现价：' + str(price) + ', 成本价：' + str(cost_price), ', 盈亏：' + str(diff_yk))
+
+                        # # 设置保本价格
+                        if price > cost_price * 1.008 and bb_price == 0.0:
+                            bb_price = 1.002 * price
 
                         # 设置最大价格
                         if price > cost_price * 1.015 and max_price == 0.0:
@@ -119,6 +139,15 @@ def online_one_price(my_position):
                                 flag = False
                                 break
 
+                        # 保本0.02
+                        if price <= bb_price and max_price == 0.0:
+                            gd_price = round(cost_price * 0.95, 2)
+                            # 挂 -5% 清仓
+                            user.sell(stock_code, price=gd_price, amount=enable_amount)
+                            print('保本')
+                            flag = False
+                            break
+
                         # 止损价 -1%
                         if price <= cost_price * 0.99:
                             gd_price = round(cost_price * 0.95, 2)
@@ -133,14 +162,17 @@ def online_one_price(my_position):
             time.sleep(3)
 
 
-# def test():
-#     user.sell('123177', price=100, amount=10)
+def test():
+    # user.sell('123177', price=100, amount=10)
+    user.current_deal
+    # user.today_entrusts
 
 
 if __name__ == '__main__':
-    get_user()
+    # get_user()
     # print(str(get_balance()))
     # position_info()
     # online_one_price('')
     sell_info()
     # test()
+    # current_deal_info()

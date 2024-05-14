696fd26d6dd81c80e00fd4dddb8b486c4f515647
jcl <1427682825@qq.com>
2024-05-14 16:57:24 +0800

    采集跌幅

--- zhuanzai/zhuanzai_auto.py
+++ zhuanzai/zhuanzai_auto.py
@@ -5,21 +5,24 @@ from mootdx.quotes import Quotes
 import easytrader
 from datetime import datetime
 
-
 tdx_client = Quotes.factory(market='std')
 # 初始化账号
 user = easytrader.use('eastmoney')
 user.prepare('account.json')
 
 # ==================================================================
-code = "118019"
-cb_price = 166.261
-enable_amount = 1330
+code = "123034"
+cb_price = 242.829
+enable_amount = 870
 min_price = 1000
 max_price = 0
 k = 0
 # 卖出标记
-sell_flag = False
+sell_flag_one = False
+sell_flag_two = False
+
+
+
 # ==================================================================
 
 
@@ -32,7 +35,7 @@ def sell(code, gd_price, enable_amount):
     user.sell(code, price=gd_price, amount=enable_amount)
 
 
-def is_data(dq_price):
+def is_data_one(dq_price):
     global k, min_price, max_price
     if dq_price < min_price:
         min_price = dq_price
@@ -46,16 +49,37 @@ def is_data(dq_price):
     return False
 
 
+def is_data_two(dq_price):
+    global k, min_price, max_price
+    if dq_price < min_price:
+        min_price = dq_price
+        k = k + 1
+    if dq_price > max_price:
+        max_price = dq_price
+        min_price = dq_price
+        k = 0
+    if k == 7:
+        return True
+    return False
+
+
 def sell_strategy(code, cb_price, enable_amount):
-    global sell_flag
+    global sell_flag_one
+    global sell_flag_two
     dq_price = get_price(code)[0]
     zf = round((dq_price - cb_price) / cb_price * 100, 2)
 
     current_time = datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
     if zf > 0.8:
-        sell_flag = True
-    if sell_flag and is_data(dq_price):
+        sell_flag_one = True
+    if zf > 1.8:
+        sell_flag_two = True
+    if sell_flag_one and is_data_one(dq_price) and not sell_flag_two:
+        gd_price = round(dq_price * 0.998, 2)
+        sell(code, gd_price, enable_amount)
+        return True
+    if sell_flag_two and is_data_two(dq_price):
         gd_price = round(dq_price * 0.998, 2)
         sell(code, gd_price, enable_amount)
         return True
@@ -64,7 +88,8 @@ def sell_strategy(code, cb_price, enable_amount):
         gd_price = round(dq_price * 0.998, 2)
         sell(code, gd_price, enable_amount)
         return True
-    print(f'时间：{formatted_time}  代码：{code}  现价：{round(dq_price, 2)}  涨幅：{zf} 计数：{k} 最小值：{round(min_price, 2)} 最大值：{round(max_price, 2)}')
+    print(
+        f'时间：{formatted_time}  代码：{code}  现价：{round(dq_price, 2)}  涨幅：{zf} 计数：{k} 最小值：{round(min_price, 2)} 最大值：{round(max_price, 2)}')
 
 
 if __name__ == '__main__':

bee508f624ebaca44bfc16013c2270453c0f9914
jcl <1427682825@qq.com>
2024-05-14 17:23:52 +0800

    增加  保本

--- zhuanzai/zhuanzai_auto.py
+++ zhuanzai/zhuanzai_auto.py
@@ -20,9 +20,9 @@ k = 0
 # 卖出标记
 sell_flag_one = False
 sell_flag_two = False
-
-
-
+# 保本
+sell_flag_bb1 = False
+sell_flag_bb2 = False
 # ==================================================================
 
 
@@ -66,6 +66,8 @@ def is_data_two(dq_price):
 def sell_strategy(code, cb_price, enable_amount):
     global sell_flag_one
     global sell_flag_two
+    global sell_flag_bb1
+    global sell_flag_bb2
     dq_price = get_price(code)[0]
     zf = round((dq_price - cb_price) / cb_price * 100, 2)
 
@@ -75,6 +77,10 @@ def sell_strategy(code, cb_price, enable_amount):
         sell_flag_one = True
     if zf > 1.8:
         sell_flag_two = True
+    if zf <= -1.5:
+        sell_flag_bb1 = True
+    if dq_price >= cb_price and sell_flag_bb1:
+        sell_flag_bb2 = True
     if sell_flag_one and is_data_one(dq_price) and not sell_flag_two:
         gd_price = round(dq_price * 0.998, 2)
         sell(code, gd_price, enable_amount)
@@ -83,13 +89,17 @@ def sell_strategy(code, cb_price, enable_amount):
         gd_price = round(dq_price * 0.998, 2)
         sell(code, gd_price, enable_amount)
         return True
+    if sell_flag_bb2:
+        if dq_price < cb_price * 1.005:
+            gd_price = round(dq_price * 0.998, 2)
+            sell(code, gd_price, enable_amount)
+            return True
     # 割肉
     if zf <= -3:
         gd_price = round(dq_price * 0.998, 2)
         sell(code, gd_price, enable_amount)
         return True
-    print(
-        f'时间：{formatted_time}  代码：{code}  现价：{round(dq_price, 2)}  涨幅：{zf} 计数：{k} 最小值：{round(min_price, 2)} 最大值：{round(max_price, 2)}')
+    print(f'时间：{formatted_time}  代码：{code}  现价：{round(dq_price, 2)}  涨幅：{zf} 计数：{k} 最小值：{round(min_price, 2)} 最大值：{round(max_price, 2)}')
 
 
 if __name__ == '__main__':
